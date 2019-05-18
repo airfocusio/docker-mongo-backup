186795811446c38e8090d62325b19f58f983707f
Christian Hoffmeister <mail@choffmeister.de>
2019-05-18 13:05:57 +0200

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+mc config host add backup ${S3_HOST} ${S3_ACCESS_KEY_ID} ${S3_ACCESS_KEY_SECRET}
+FOLDER="$(/bin/date +%Y)/$(/bin/date +%Y-%m)/$(/bin/date +%Y-%m-%d)"
+FILE="$(/bin/date +%Y-%m-%d-%H%M%S)-${DATABASE}.mongodb.gz"
+mongodump \
+  --host "${HOST}" \
+  --db "${DATABASE}" \
+  --username "${USERNAME}" \
+  --password "${PASSWORD}" \
+  --authenticationDatabase "${AUTH_DATABASE:-admin}" \
+  --gzip --archive | mc pipe "backup/${S3_DIRECTORY}/${FOLDER}/${FILE}"
