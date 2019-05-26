a73930d4f4fbfbd095f0547ee71295591cd56c1a
Christian Hoffmeister <mail@choffmeister.de>
2019-05-26 11:08:19 +0200

    Add restore

--- README.md
+++ README.md
@@ -12,5 +12,22 @@ docker run --rm \
   -e S3_ACCESS_KEY_ID=id \
   -e S3_ACCESS_KEY_SECRET=secret \
   -e S3_DIRECTORY=myspace/mongo-backup \
-  airfocusio/mongo-backup:4.0.9
+  airfocusio/mongo-backup:4.0.9 \
+  backup
+```
+
+```
+docker run --rm \
+  -e HOST=localhost \
+  -e PORT=27017 \
+  -e DATABASE=mydatabase \
+  -e USERNAME=user \
+  -e PASSWORD=pass \
+  -e AUTH_DATABASE=admin \
+  -e S3_HOST=https://fra1.digitaloceanspaces.com \
+  -e S3_ACCESS_KEY_ID=id \
+  -e S3_ACCESS_KEY_SECRET=secret \
+  -e S3_DIRECTORY=myspace/mongo-backup \
+  airfocusio/mongo-backup:4.0.9 \
+  restore 2019/2019-01/2019-01-01/2019-01-01-010203-mydatabase.mongodb.gz
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 set -e
 
-mc config host add backup ${S3_HOST} ${S3_ACCESS_KEY_ID} ${S3_ACCESS_KEY_SECRET}
-FOLDER="$(/bin/date +%Y)/$(/bin/date +%Y-%m)/$(/bin/date +%Y-%m-%d)"
-FILE="$(/bin/date +%Y-%m-%d-%H%M%S)-${DATABASE}.mongodb.gz"
-mongodump \
-  --host "${HOST}" \
-  --port "${PORT:-27017}" \
-  --db "${DATABASE}" \
-  --username "${USERNAME}" \
-  --password "${PASSWORD}" \
-  --authenticationDatabase "${AUTH_DATABASE:-admin}" \
-  --gzip --archive | mc pipe "backup/${S3_DIRECTORY}/${FOLDER}/${FILE}"
+if [ "$1" == "backup" ]; then
+  mc config host add backup ${S3_HOST} ${S3_ACCESS_KEY_ID} ${S3_ACCESS_KEY_SECRET}
+  FOLDER="$(/bin/date +%Y)/$(/bin/date +%Y-%m)/$(/bin/date +%Y-%m-%d)"
+  FILE="$(/bin/date +%Y-%m-%d-%H%M%S)-${DATABASE}.mongodb.gz"
+  mongodump \
+    --host "${HOST}" \
+    --port "${PORT:-27017}" \
+    --db "${DATABASE}" \
+    --username "${USERNAME}" \
+    --password "${PASSWORD}" \
+    --authenticationDatabase "${AUTH_DATABASE:-admin}" \
+    --gzip --archive | mc pipe "backup/${S3_DIRECTORY}/${FOLDER}/${FILE}"
+elif [ "$1" == "restore" ]; then
+  mc config host add backup ${S3_HOST} ${S3_ACCESS_KEY_ID} ${S3_ACCESS_KEY_SECRET}
+  mc cat "backup/${S3_DIRECTORY}/$2" | mongorestore \
+    --host "${HOST}" \
+    --port "${PORT:-27017}" \
+    --username "${USERNAME}" \
+    --password "${PASSWORD}" \
+    --authenticationDatabase "${AUTH_DATABASE:-admin}" \
+    --gzip --archive --drop
+else
+  echo "Mode must be backup or restore!"
+  exit 1
+fi
